806ad14fae518b8dac75e966a9beed12a429600f
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-03-23 12:16:55 +0100

    seed data added

--- seeders/20200323101009-some-homepages.js
+++ seeders/20200323101009-some-homepages.js
@@ -6,39 +6,30 @@ module.exports = {
       "homepages",
       [
         {
-          title: "testuser",
-          description: "test@test.com",
-          backgroundColor: 'ffff',
-          color: dddd,
-          userId: xxxx,
+          title: "testhomepage1",
+          description: "a random description of stuff",
+          backgroundColor: '#121629',
+          color: '#fffffe',
+          userId: 1,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
-          title: "testuser",
-          description: "test@test.com",
-          backgroundColor: 'ffff',
-          color: dddd,
-          userId: xxxx,
+          title: "testhomepage2",
+          description: "a couple of things that i want to broadcast to the world",
+          backgroundColor: '#eff0f3',
+          color: '#ff8e3c',
+          userId: 2,
           createdAt: new Date(),
           updatedAt: new Date()
-        },
-        {
-          title: "testuser",
-          description: "test@test.com",
-          backgroundColor: 'ffff',
-          color: dddd,
-          userId: xxxx,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
+        }
       ],
       {}
     );
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete("users", null, {});
+    return queryInterface.bulkDelete("homepages", null, {});
   }
 };
 
--- seeders/20200323101014-some-stories.js
+++ seeders/20200323101014-some-stories.js
@@ -2,25 +2,47 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert(
+      "stories",
+      [
+        {
+          name: "story 1",
+          content: "a story about nothing",
+          imageUrl: 'https://lh3.googleusercontent.com/afS719gxko5j3EkeHIPpDbSP4HjwTG7-pHn3URfzQ7wPXquVX6uV6VeEnFCmkQwk4w',
+          homepageId: '2',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "story 2",
+          content: "still nothing",
+          imageUrl: 'https://image.winudf.com/v2/image/bmljb2xhaS5ub3RoaW5nX3NjcmVlbl8wXzE1MTY4MjkxOTlfMDA2/screen-0.jpg?fakeurl=1&type=.jpg',
+          homepageId: '1',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "story 3",
+          content: "nothing you say?",
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/84/Nothing_silhouette.jpg',
+          homepageId: '2',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "story 4",
+          content: "it's nothing personal",
+          imageUrl: 'https://res.cloudinary.com/teepublic/image/private/s--QHUeYZ_G--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1553023888/production/designs/4449105_0.jpg',
+          homepageId: '1',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete("stories", null, {});
   }
 };
